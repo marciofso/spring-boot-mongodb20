67bc667bc63a609737d3273e5032dd8aa432b690
Marcio Fernandes <marciofso@hotmail.com>
2020-10-16 08:04:19 -0300

    Projeto criado

---
+++ src/main/java/com/marciofso/springbootmongo/SpringBootMongodb20Application.java
@@ -0,0 +1,13 @@
+package com.marciofso.springbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodb20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodb20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marciofso/springbootmongo/SpringBootMongodb20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciofso.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodb20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
