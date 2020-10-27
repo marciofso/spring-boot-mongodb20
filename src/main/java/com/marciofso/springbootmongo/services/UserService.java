08cf517e30cfa5694194492561f512a75e0a5bf1
Marcio Fernandes <marciofso@hotmail.com>
2020-10-27 15:03:09 -0300

    obtendo um usuario por id

--- src/main/java/com/marciofso/springbootmongo/services/UserService.java
+++ src/main/java/com/marciofso/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marciofso.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marciofso.springbootmongo.domain.User;
 import com.marciofso.springbootmongo.repository.UserRepository;
+import com.marciofso.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,10 @@ public class UserService {
 		return repo.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	
+	}
 
 }
