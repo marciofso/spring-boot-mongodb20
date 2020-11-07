4127fa6c8306a5738af98deb1f28b0c03db71c13
Marcio Fernandes <marciofso@hotmail.com>
2020-11-07 14:53:55 -0300

    Consulta simples com @Query

--- src/main/java/com/marciofso/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/marciofso/springbootmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marciofso.springbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marciofso.springbootmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.marciofso.springbootmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
  
 }
